8aec298dda80f5865f2679e70c73364e87dac014
nathanieleh <nathanieleh@gmail.com>
2023-05-03 20:24:56 -0700

    title
    ig I have to do this but the file size will be large w/o cssnano i think

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 // initialize modules
 const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
+//const postcss = require('gulp-postcss');
+//const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 const babel = require('gulp-babel');
 const terser = require('gulp-terser');
